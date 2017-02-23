40fe6ab363f4e8a6af1315a13fb68d2b8d68bef9
Ruggero Turra <giurrero@gmail.com>
2017-02-23 08:27:31 +0100

    Improved script, more generric, removed python setup (conda).
    Port number is randomly generated, hostname is
    automatically set, message with instruction for local host.
    Removed the conda setup since it should be done before by the user.

--- notebook.sh
+++ notebook.sh
@@ -7,19 +7,27 @@ if [ ! -z "$1" ]; then
 fi
 
 if [ -z "$ssh_port" ]; then
-  ssh_port=55577
+  ssh_port=`python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()'`
   echo "PORT NOT SET: Using port $ssh_port"
 fi
 
-echo "Tunneling through port $ssh_port"
+host_name=`hostname -f`
 
-# load conda environment 
-module load Langs/Python/3.5-anaconda
+echo "Tunneling through port $ssh_port from $host_name"
 
-# setup tunnel 
-ssh -f -o ExitOnForwardFailure=yes atlas1.hep.yale.edu -R ${ssh_port}:localhost:${ssh_port} sleep 30
+# setup tunnel
+#ssh -f -o ExitOnForwardFailure=yes ${host_name} -R ${ssh_port}:localhost:${ssh_port} sleep 30
 
 # launch notebook
-jupyter notebook --no-browser --port=${ssh_port}
+#jupyter notebook --no-browser --port=${ssh_port} --ip 127.0.0.1
+
+echo -e "\nRun this command on your client:"
+echo -e "\n  ssh -N -f -L $ssh_port:localhost:$ssh_port $host_name"
+echo -e "\nand open a browser to http://localhost:$ssh_port"
+echo
+
+if [ -z `hostname -d` ]; then
+    echo "domain is missing from the hostname $host_name, you have to add it"
+fi
 
 unset ssh_port
